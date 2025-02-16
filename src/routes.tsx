0cdada38db4358bd73de87ce190ed4b86cd5c19b
ricardoareiaslinhares <ricardolinhares.dev@gmail.com>
2025-02-16 22:58:26 +0000

    Routes

---
+++ src/routes.tsx
@@ -0,0 +1,23 @@
+import Home from "./pages/Home";
+import { createBrowserRouter, RouteObject } from "react-router-dom";
+import NotFoundError from "./pages/NotFoundError";
+import Clients from "./pages/Clients";
+import ClientDetails from "./pages/ClientDetails";
+import Layout from "./Layout";
+
+const routes: RouteObject[] = [
+    {
+        path: "/",
+        element: <Layout />,
+        errorElement: <NotFoundError />,
+        children: [
+            {index: true, element: <Home />},
+            {path: "/clients", element: <Clients />},
+            {path: "/clients/:id", element: <ClientDetails />},
+        ]
+    },
+
+];
+
+const router = createBrowserRouter(routes);
+export default router;
