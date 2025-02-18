64986fab908d94a8e922c32dbe8fbdc4db864684
ricardoareiaslinhares <ricardolinhares.dev@gmail.com>
2025-02-18 03:51:33 +0000

    small changes

--- src/pages/ClientDetails.tsx
+++ src/pages/ClientDetails.tsx
@@ -1,13 +1,17 @@
+import { Box, Typography } from "@mui/material";
 import { useParams } from "react-router-dom";
 
 type Props = {}
 
 const ClientDetails = (props: Props) => {
   const { id } = useParams();
-  if (!id) return <p>CLiente não existe!</p>;
+  if (!id) return <p>Cliente não existe!</p>;
 
   return (
-    <div>ClientDetails id: {id}</div>
+
+    <Box sx={{ display: "flex", flex: 1, flexDirection: "column", padding:1 }}>
+      <Typography>Clientid: {id}</Typography>
+    </Box>
   )
 }
 
