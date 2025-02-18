e1d9e63b154518d4a8143d0950ad65d9e733fa54
ricardoareiaslinhares <ricardolinhares.dev@gmail.com>
2025-02-18 03:36:21 +0000

    Layout: clean up

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -4,33 +4,36 @@ import { Outlet } from "react-router";
 import DrawerCustom from "./components/SideBar/DrawerCustom";
 import { Box, styled } from "@mui/material";
 
+
+const DRAWER_WIDTH = 240;
+
+const Main = styled("main", {
+  shouldForwardProp: (prop) => prop !== "open",
+})<{
+  open?: boolean;
+}>(({ theme, open }) => ({
+  display: "flex",
+  flex: 1,
+  flexGrow: 1,
+  marginTop: "70px",
+  minHeight: "calc(100vh - 70px)",
+  flexDirection: "column",
+  justifyContent: "space-between",
+
+  paddingInline: 2,
+  marginLeft: open ? 0 : `-${DRAWER_WIDTH}px`,
+  transition: theme.transitions.create("margin", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+}));
+
 const Layout = () => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(true);
-  const drawerWidth = 240;
+
   const handleDrawer = useCallback(() => {
     setIsDrawerOpen((prev) => !prev);
   }, []);
-
-  const Main = styled("main", {
-    shouldForwardProp: (prop) => prop !== "open",
-  })<{
-    open?: boolean;
-  }>(({ theme, open }) => ({
-    display: "flex",
-    flex: 1,
-    flexGrow: 1,
-    marginTop:"70px",
-    minHeight: "calc(100vh - 70px)",
-    flexDirection: "column",
-    justifyContent: "space-between",
-
-    paddingInline: 2,
-    marginLeft: open ? 0 : `-${drawerWidth}px`,
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  }));
 
   return (
     <Box
@@ -46,14 +49,20 @@ const Layout = () => {
       <DrawerCustom
         open={isDrawerOpen}
         handleDrawer={handleDrawer}
-        drawerWidth={drawerWidth}
+        drawerWidth={DRAWER_WIDTH}
       >
         <Main open={isDrawerOpen}>
           <div>
-
-          <Outlet />
+            <Outlet />
           </div>
-          <Box sx={{bgcolor:"yellow", display:"flex", justifyContent:"center", padding:1}}>
+          <Box
+            sx={{
+              bgcolor: "yellow",
+              display: "flex",
+              justifyContent: "center",
+              padding: 1,
+            }}
+          >
             <h3>Footer</h3>
           </Box>
         </Main>
