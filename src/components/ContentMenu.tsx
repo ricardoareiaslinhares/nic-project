3ee6f621b008a40cdddeb2cc2f43b5af29512908
ricardoareiaslinhares <ricardolinhares.dev@gmail.com>
2025-02-22 20:11:46 +0000

    Changed searchBar to be dynamic

--- src/components/ContentMenu.tsx
+++ src/components/ContentMenu.tsx
@@ -3,7 +3,7 @@ import SearchBar from './SearchBar'
 import AddNew from './AddNew'
 
 type Props = {
-  handleFilteredData:  (input: string) => void;
+  handleFilteredData?:  (input: string) => void;
   searchPlaceHolder?: string
   addNewClick: () => void
 }
@@ -12,7 +12,9 @@ const ContentMenu = ({handleFilteredData, searchPlaceHolder, addNewClick}: Props
   return (
     <Box sx={{display:"flex", flex:1, alignItems:"center", flexDirection:"row", justifyContent:"start" }}>
         <div>
+          {handleFilteredData &&
         <SearchBar handleFilteredData={handleFilteredData} placeHolder={searchPlaceHolder}/>
+          }
         </div>
         <AddNew onClick={addNewClick}/>
     </Box>
