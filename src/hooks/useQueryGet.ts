688f9215784fb7d984f1f90287bdfb8fc88b7d7a
ricardoareiaslinhares <ricardolinhares.dev@gmail.com>
2025-02-23 16:19:11 +0000

    Modifyied query to use select (only return selected proprieties of the entity

--- src/hooks/useQueryGet.ts
+++ src/hooks/useQueryGet.ts
@@ -1,17 +1,25 @@
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import {
+  useQuery,
+  useQueryClient,
+  UseQueryOptions,
+} from "@tanstack/react-query";
 
-type Props<T> = {
-    getFn: () => Promise<T[]>;
-    queryKey: string
-}
+type Props<T, TSelected = T> = {
+  getFn: () => Promise<T[]>;
+  queryKey: string;
+} & Omit<UseQueryOptions<T[], unknown, TSelected>, "queryKey" | "queryFn">;
 
-const useQueryGet = <T,>({ getFn, queryKey }: Props<T>) => {
-    const queryClient = useQueryClient();
-    return useQuery({
-        queryKey: [queryKey],
-        queryFn: () => getFn(),
+const useQueryGet = <T, TSelected>({
+  getFn,
+  queryKey,
+  ...rest
+}: Props<T, TSelected>) => {
+  // const queryClient = useQueryClient();
+  return useQuery({
+    queryKey: [queryKey],
+    queryFn: () => getFn(),
+    ...rest,
+  });
+};
 
-    });
-}
-
-export default useQueryGet
+export default useQueryGet;
