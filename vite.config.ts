5c7f0d4e27978c2ed91d3f556f2eeb3273b0ea73
ricardoareiaslinhares <ricardolinhares.dev@gmail.com>
2025-02-25 22:43:55 +0000

    prod changes

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,23 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),
     tailwindcss(),
-  ],
+  ],/*
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("react")) return "react-vendor";
+            if (id.includes("@tanstack/react-query")) return "tanstack-query-vendor";
+            if (id.includes("react-hook-form") || id.includes("formik")) return "forms-vendor";
+            if (id.includes("@mui")) return "mui-vendor";
+            return "vendor"; // Default bucket for other libs
+          }
+        },
+      },
+    }
+  }
+    */
+  
 })
+
