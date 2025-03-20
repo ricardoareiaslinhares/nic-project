277c9bbc04b24795268b74d588c1e65ec54398be
Ricardo Linhares NI <ricardo.linhares@neuroimprove.pt>
2025-03-20 17:43:36 +0000

    rfc- added a clientRaw type, before transform that adds name (fullname)

--- src/types/client.ts
+++ src/types/client.ts
@@ -1,6 +1,18 @@
+export type ClientRaw = {
+  id: number;
+  psychologist: number;
+  user: {
+    id: string;
+    first_name: string;
+    last_name: string;
+    email: string;
+  };
+};
+
 export type Client = {
   id: number;
   psychologist: number;
+  name: string;
   user: {
     id: string;
     first_name: string;
