b8d404ab82f43f4c6be2056aef9bca6cdd2b85f4
Ricardo Linhares NI <ricardo.linhares@neuroimprove.pt>
2025-03-20 15:24:13 +0000

    SandBox for Notes done

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,19 +4,22 @@ import NotFoundError from "./pages/NotFoundError";
 import Clients from "./pages/clients/Clients";
 import ClientDetails from "./pages/clientDetails/ClientDetails";
 import Layout from "./Layout";
+import { UserProfile } from "./pages/userProfile/UserProfile";
+import { NotesScreen } from "./sand-box/NotesScreen";
 
 const routes: RouteObject[] = [
-    {
-        path: "/",
-        element: <Layout />,
-        errorElement: <NotFoundError />,
-        children: [
-            {index: true, element: <Home />},
-            {path: "/clients", element: <Clients />},
-            {path: "/clients/:id", element: <ClientDetails />},
-        ]
-    },
-
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <NotFoundError />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "/user", element: <UserProfile /> },
+      { path: "/clients", element: <Clients /> },
+      { path: "/clients/:id", element: <ClientDetails /> },
+      { path: "sandbox", element: <NotesScreen /> },
+    ],
+  },
 ];
 
 const router = createBrowserRouter(routes);
