d3a6b053f5415f114e0ae83a0e6745402cf63281
Ricardo Linhares NI <ricardo.linhares@neuroimprove.pt>
2025-03-27 09:52:05 +0000

    rfc- all client logic and prep of presentation (dataGrid columns) into screens/clients

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,35 +1,10 @@
 import Home from "./pages/Home";
 import { createBrowserRouter, RouteObject } from "react-router-dom";
 import NotFoundError from "./pages/NotFoundError";
-import Clients from "./pages/clients/Clients";
 import Layout from "./Layout";
 import { UserProfile } from "./pages/userProfile/UserProfile";
 import { NotesScreen } from "./sand-box/NotesScreen";
-import { Record } from "./components/record/Record";
-import { Records } from "./components/records/Records";
-import {
-  clientDetailsScreenConfig,
-  clientsScreenConfig,
-} from "./api_2/clientsConfig";
-import { Client } from "./types/entities/client";
-import { ClientDetails } from "./pages/clientDetailsNew/ClientDetails";
-
-const clientsScreen = {
-  path: "/clients",
-  element: (
-    <Records recordConfig={clientsScreenConfig} children={<Clients />} />
-  ),
-};
-
-const clientDetailsScreen = {
-  path: "/clients/:id",
-  element: (
-    <Record
-      recordConfig={clientDetailsScreenConfig}
-      children={<ClientDetails />}
-    />
-  ),
-};
+import { clientRoutes } from "./screens/clients/clientRoutes";
 
 const routes: RouteObject[] = [
   {
@@ -39,9 +14,8 @@ const routes: RouteObject[] = [
     children: [
       { index: true, element: <Home /> },
       { path: "/user", element: <UserProfile /> },
-      { path: clientsScreen.path, element: clientsScreen.element },
-      { path: clientDetailsScreen.path, element: clientDetailsScreen.element },
       { path: "/sandbox", element: <NotesScreen /> },
+      ...clientRoutes, // TODO Do Auth
     ],
   },
 ];
