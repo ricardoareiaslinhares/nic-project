bfa4c5b748ae1fd3b68a6387b919148d6ceab833
Ricardo Linhares-NI <ricardo.linhares@neuroimprove.pt>
2025-05-20 09:32:59 +0100

    data grid options

--- src/screens/clients/clientsOptions.ts
+++ src/screens/clients/clientsOptions.ts
@@ -3,6 +3,7 @@ import { DataGridOptions } from "../../types/dataGrid.types";
 export const clientsOptions: DataGridOptions = {
   dataGridProps: {
     //onRowDoubleClick: (params) => console.log("clicked", params.row),
+
   },
   extraOptions: {
     navigateDetails: "/clients/",
